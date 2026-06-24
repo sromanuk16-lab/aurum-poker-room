ede3cf02e45c7c48135097029f9a4017a2464c28
sromanuk16-lab <sromanuk16@gmail.com>
2026-06-24 22:27:05 +0300

    feat: add premium event constants

--- src/content/premiumEvents.js
+++ src/content/premiumEvents.js
@@ -1,4 +1,16 @@
 export const PREMIUM_EVENT_TYPE = 'premium_event';
+export const DEALER_REACTION_TYPE = 'dealer_reaction';
+export const EVENT_TIMING_AFTER_HAND = 'after_hand';
+export const EVENT_SEATS = Object.freeze([0, 1, 2, 3, 4]);
+
 export function isPremiumEvent(item) {
   return Boolean(item && item.type === PREMIUM_EVENT_TYPE);
+}
+
+export function isDealerReaction(item) {
+  return Boolean(isPremiumEvent(item) && item.eventKind === DEALER_REACTION_TYPE);
+}
+
+export function requiredDealerReactionAssets() {
+  return ['preview', 'poster', 'seat_0', 'seat_1', 'seat_2', 'seat_3', 'seat_4'];
 }
